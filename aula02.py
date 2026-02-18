6fb897ec4beaba8133834304a7d054494399416f
Cleo Maria <cleo.maria2805@gmail.com>
2026-02-18 12:44:03 -0300

    primeira aula de programação

---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 536 # variavel int
+numero_quebrado = 9.56 # variavel float
+toda_segunda_tem_aula_de_fisica = True # variavel booleana
+texto = "esta ficando muito calor" # variavel de texto
+
+
+print(toda_segunda_tem_aula_de_fisica)
+print(numero)
+print(numero_quebrado)
+print(texto)
