3e3f9344e61cde4db9582825ef156871ded86fe1
cleo maria <cleo.maria2805@gmail.com>
2026-02-20 09:49:35 -0300

    aula com codigos de matematica

---
+++ matematica.py
@@ -0,0 +1,13 @@
+valor1 = int(input("digite o valor: "))
+valor2 = int(input("digite o valor: "))
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+divisao = valor1 / valor2
+multiplicacao = valor1 * valor2
+
+print("A soma é: ", soma)
+print("A subtração é: ", subtracao)
+print("A divisão é: ", divisao)
+print("A multiplicação é: ", multiplicacao)
+
