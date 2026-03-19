ae8103e0e18bf9c9fcd05ca464a360e3eece4048
cleo maria <cleo.maria2805@gmail.com>
2026-03-19 10:04:52 -0300

    19/03

---
+++ playlis.py
@@ -0,0 +1,5 @@
+playlis = ["Musica 1", "Musica 2", "Musica 3", "Musica 4", "Musica 5"]
+print(f"a playlist atual é: {playlis}")
+playlis.remove("Musica 3")
+playlis.pop(0)
+print(f"a playlist agora é: {playlis}")
---
+++ vagas.py
@@ -0,0 +1,4 @@
+vagas = ["livre", "ocupado", "livre", "ocupado"]
+escolha = int(input("digite um numero de 0 a 3: "))
+if escolha % 2 == 0 and vagas == "livre":
+    print(f"vagas{}")
