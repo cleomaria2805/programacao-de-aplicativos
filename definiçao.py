37114eba6fafcf61493c52abdd50e65b04dac85e
cleo maria <cleo.maria2805@gmail.com>
2026-04-15 12:18:25 -0300

    15/04

---
+++ definiçao.py
@@ -0,0 +1,8 @@
+def exibir_cabecalho():
+
+    print("---- SISTEMA DE CADASTRO ----")
+    print("1. inserir cliente")
+    print("2. sair")
+
+print("Iniciando programa...")
+print("Fim.")
---
+++ parametros.py
@@ -0,0 +1,5 @@
+def exibir_saudacao(nome, idade):
+    print(f"Olá, {nome}! Você tem {idade} anos.")
+
+exibir_saudacao("Maria", 25)
+exibir_saudacao("João", 30)
