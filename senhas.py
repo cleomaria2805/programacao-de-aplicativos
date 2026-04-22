59d1b85bc9aba746353199a3198711e05c3d50fb
cleo maria <cleo.maria2805@gmail.com>
2026-04-22 12:11:43 -0300

    22/04

---
+++ senhas.py
@@ -0,0 +1,10 @@
+def senha_valida(senha_correta):
+    while len(senha_valida) < 6:
+        senha_correta = input("digite a senha")
+        if len(senha_correta) >= 6
+        print(True) 
+    else:
+        return(False)
+     
+senha_usuario = int(input("digite sua senha: "))
+senha_valida = (senha_usuario)
